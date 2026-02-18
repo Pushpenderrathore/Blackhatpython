b210857e69ce98221133de42e37978caeee9414c
Pushpenderrathore <pushpendersingh046@gmail.com>
2026-02-19 00:09:54 +0530

    Modified

--- tcpclient.py
+++ tcpclient.py
@@ -1,10 +1,38 @@
 import socket
+import argparse
 
-def tcpclient(target: str,port: int,message: str):
-    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    client.connect((target,port))
-    client.send(message.encode())
-    data = client.recv(4096) 
-    print(data.decode())
+class TCPClient:
+    def __init__(self, target: str, port: int, timeout=5):
+        self.target = target
+        self.port = port
+        self.timeout = timeout
 
-tcpclient("google.com",80,"GET / HTTP/1.1\r\nHOST : google.com\r\n\r\n")
+    def run(self, data):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+            client.settimeout(self.timeout)
+            client.connect((self.target, self.port))
+            client.sendall(data.encode())
+            try:
+                response = client.recv(4096)
+                return response.decode(errors="ignore")
+            except socket.timeout:
+                return "[!] No response (timeout)"
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Simple TCP Client")
+    parser.add_argument("-t", "--target", required=True, help="Target Host")
+    parser.add_argument("-p", "--port", required=True, type=int, help="Target Port")
+    parser.add_argument("-m", "--message", required=True, help="Message to send")
+
+    args = parser.parse_args()
+
+    client = TCPClient(args.target, args.port)
+    response = client.run(args.message)
+
+    print("[+] Response:")
+    print(response)
+
+
+if __name__ == "__main__":
+    main()

0e3a128a146928403948cc98d84fbf4d0d019f89
Pushpenderrathore <pushpendersingh046@gmail.com>
2026-02-27 06:43:59 +0530

    Modified

--- TUClient.py
+++ TUClient.py
@@ -1,7 +1,6 @@
 import socket
 import argparse
 import os
-import threading
 
 class TUClient:
     def __init__(self,target:str,port:int,message:str,protocol:str="tcp",timeout:int=5):
@@ -14,11 +13,9 @@ class TUClient:
     def connection(self):
         if self.protocol=="tcp":
             return self._run_tcp()
-        elif self.protocol=="udp":
+        if self.protocol=="udp":
             return self._run_udp()
-        else:
-            return "Unknown command use [tcp or udp]"
-
+        
     def _run_tcp(self):
         try:
             with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as client:
@@ -27,53 +24,49 @@ class TUClient:
                 client.sendall(self.message)
                 resp = client.recv(4096)
                 return resp.decode(errors="ignore")
-
         except socket.timeout:
-            return "Connection: Time Out"
+            return "[!] Connection Time Out"
         except ConnectionRefusedError:
-            return "Host: down/filter"
+            return "[!] Connection Refused"
         except KeyboardInterrupt:
-            return "User: Interrupted"
+            return "[!] User Interrupted"
         except socket.gaierror:
-            return "Host: Unknown"
+            return "[!] Unknown Host"
         except Exception as e:
-            return "Unexpected Error!"
-
+            return "[!] Unexpected Error"
+        
     def _run_udp(self):
         try:
             with socket.socket(socket.AF_INET,socket.SOCK_DGRAM) as client:
                 client.settimeout(self.timeout)
-                client.sendto("Hello",(self.target,self.port))
-                data , resp = client.recvfrom(4096)
-                return (f"response from {data} -> resp")
-
+                client.sendto(self.message,(self.target,self.port))
+                data , addr = client.recvfrom(4096)
+                return f"Response from {self.target}:{self.port} -> {data.decode(errors='ignore')}:{addr}"
         except socket.timeout:
-            return "Connection: Time Out"
+            return "[!] Connection Time Out"
         except ConnectionRefusedError:
-            return "Port: closed"
+            return "[!] Connection Refused"
         except KeyboardInterrupt:
-            return "User: Interrupted"
+            return "[!] User Interrupted"
         except socket.gaierror:
-            return "Host: Unknown"
+            return "[!] Unknown Host"
         except Exception as e:
-            return "Unexpected Error!"
-
+            return "[!] Unexpected Error"
+        
 def main():
     parser = argparse.ArgumentParser(description="TU Client")
-    parser.add_argument("-t","--target",required=True,help="Target host")
-    parser.add_argument("-p","--port",type=int,required=True,help="Target port")
-    parser.add_argument("-m","--message",type=str,required=True,help="Message to send")
-    parser.add_argument("-P","--protocol",type=str,required=True,default="tcp",help="Use from tcp or udp")
-    parser.add_argument("-T","--timeout",type=int,required=True,default=True,help="set time out")
+    parser.add_argument("-t","--target",required=True,help="Target Host")
+    parser.add_argument("-p","--port",required=True,type=int,help="Target Port")
+    parser.add_argument("-m","--message",required=True,type=str,help="Message To Send")
+    parser.add_argument("-P","--protocol",choices=["tcp" , "udp"],default="tcp",help="protocol tcp or udp")
+    parser.add_argument("-T","--timeout",type=int,default=5,help="Set Time Out")
 
     args = parser.parse_args()
 
     client = TUClient(args.target,args.port,args.message,args.protocol,args.timeout)
     resp = client.connection()
 
-    print (f"Response from {args.target}:{args.port} -> {resp}")
+    print(f"Response from {args.target}:{args.port} -> {resp}")
 
 if __name__=="__main__":
-    main()
-
-
+    main()        
